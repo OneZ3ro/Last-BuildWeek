616eeaf7de8e79f4a8eaeca1c24f86e962162f8c
Angelo Moreno <emilioangelo665@gmail.com>
2023-11-20 10:10:10 +0100

    Creazione progetto

---
+++ src/test/java/group4/LastBuildWeek/LastBuildWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package group4.LastBuildWeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastBuildWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
