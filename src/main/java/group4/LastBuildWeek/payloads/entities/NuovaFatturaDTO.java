b40311dd0714361b89046b325902dea757c2274c
Angelo Moreno <emilioangelo665@gmail.com>
2023-11-21 17:25:20 +0100

    Corretto endpoint fatture

--- src/main/java/group4/LastBuildWeek/payloads/entities/NuovaFatturaDTO.java
+++ src/main/java/group4/LastBuildWeek/payloads/entities/NuovaFatturaDTO.java
@@ -1,14 +1,13 @@
 package group4.LastBuildWeek.payloads.entities;
 
-import group4.LastBuildWeek.entities.Cliente;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
-import java.time.LocalDate;
 
 public record NuovaFatturaDTO(
         @NotEmpty(message = "Il campo è obbligatorio!")
         String numeroFattura,
-        @NotEmpty(message = "Il campo è obbligatorio!")
+        @NotNull(message = "Il campo è obbligatorio!")
         double importoFattura,
-        @NotEmpty(message = "Il campo è obbligatorio!")
-        Cliente cliente) {}
+        @NotNull(message = "Il campo è obbligatorio!")
+        long clienteId) {}
