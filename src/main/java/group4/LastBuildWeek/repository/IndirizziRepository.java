1ae3268744695e04d83c7a701927fd0c54859bdc
Angelo Moreno <emilioangelo665@gmail.com>
2023-11-22 14:56:53 +0100

    Creati endpoint per provincie, comuni e indirizzi

--- src/main/java/group4/LastBuildWeek/repository/IndirizziRepository.java
+++ src/main/java/group4/LastBuildWeek/repository/IndirizziRepository.java
@@ -4,6 +4,10 @@ import group4.LastBuildWeek.entities.Indirizzi;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface IndirizziRepository extends JpaRepository<Indirizzi, Long> {
+    Optional<List<Indirizzi>> findByProvincia(String provincia);
 }
