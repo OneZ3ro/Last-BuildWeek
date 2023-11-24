06ba0ce6ec3f81808b876752d57b0acd45be4816
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2023-11-24 10:00:49 +0100

    Runner

--- src/main/java/group4/LastBuildWeek/MyRunner.java
+++ src/main/java/group4/LastBuildWeek/MyRunner.java
@@ -1,9 +1,18 @@
 package group4.LastBuildWeek;
 
 import group4.LastBuildWeek.entities.Comune;
+import group4.LastBuildWeek.entities.Indirizzi;
 import group4.LastBuildWeek.entities.Provincia;
+import group4.LastBuildWeek.enums.StatoFattura;
+import group4.LastBuildWeek.enums.TipoCliente;
+import group4.LastBuildWeek.payloads.entities.ClienteDTO;
+import group4.LastBuildWeek.payloads.entities.NewIndirizziDTO;
+import group4.LastBuildWeek.payloads.entities.NuovaFatturaDTO;
 import group4.LastBuildWeek.repository.ComuneRepository;
 import group4.LastBuildWeek.repository.ProvinciaRepository;
+import group4.LastBuildWeek.services.ClienteService;
+import group4.LastBuildWeek.services.FattureService;
+import group4.LastBuildWeek.services.IndirizziService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,6 +20,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.time.LocalDate;
 
 @Component
 @Slf4j
@@ -20,6 +30,13 @@ public class MyRunner implements CommandLineRunner {
 
     @Autowired
     private ComuneRepository comuneRepository;
+
+    @Autowired
+    private IndirizziService indirizziService;
+    @Autowired
+    private ClienteService clienteService;
+    @Autowired
+    private FattureService fattureService;
 
     @Override
     public void run(String... args) throws Exception {
@@ -36,7 +53,7 @@ public class MyRunner implements CommandLineRunner {
             while ((lineProvincia = readerProvincia.readLine()) != null) {
                 String[] row = lineProvincia.split(";");
                 Provincia provincia = new Provincia(row[0], row[1], row[2]);
-//                provinciaRepository.save(provincia);
+//               provinciaRepository.save(provincia);
             }
             while ((lineComune = readerComune.readLine()) != null) {
                 String[] row = lineComune.split(";");
@@ -53,9 +70,34 @@ public class MyRunner implements CommandLineRunner {
                 if (row.length == 4) {
                     Provincia p = provinciaRepository.findByProvincia(row[3]).orElse(null);
                     Comune comune = new Comune(row[0], row[1], row[2], row[3], p);
-//                    comuneRepository.save(comune);
+//                   comuneRepository.save(comune);
                 }
+                //indirizzi
+                //clienti con indirizzo
+                //fattura con cliente
 
+            }
+            //scommentare una linea alla volta
+            for(int i = 1;i<100;i++){
+            //    NewIndirizziDTO indirizzi = new NewIndirizziDTO("Via "+i,"Civico "+i,"cap"+ i, "Torino",i);
+              //  NewIndirizziDTO indirizzi = new NewIndirizziDTO("Via "+i,"Civico "+i,"cap"+ i, "Cuneo",i+500);
+                //NewIndirizziDTO indirizzi = new NewIndirizziDTO("Via "+i,"Civico "+i,"cap"+ i, "Alessandria",i+900);
+                //NewIndirizziDTO indirizzi = new NewIndirizziDTO("Via "+i,"Civico "+i,"cap"+ i, "Bergamo",i+1800);
+                //NewIndirizziDTO indirizzi = new NewIndirizziDTO("Via "+i,"Civico "+i,"cap"+ i, "Brescia",i+2000);
+              //  indirizziService.save(indirizzi);
+            }
+            //inserire dopo indirizzi, cambiare indice(+50) e tipo cliente
+            for(int i =1 ;i<50;i++){
+                ClienteDTO clienteDTO = new ClienteDTO(TipoCliente.SRL,"ragioneSociale"+TipoCliente.PA+i,"partitaIva"+TipoCliente.PA+i,"email"+i+"@gmail.com",
+                        LocalDate.now().minusDays(i+i), LocalDate.now().minusDays(i),100+i,"pec"+i+"@gmail.com","888777333"+i,"email"+i+"@gmail.com",
+                        "nome"+i,"cognome"+i,"888777333"+i,"",i+85552,i+85552);
+
+               // clienteService.saveCliente(clienteDTO);
+            }
+            //inserire doipo indirizzi e cliente
+            for(int i =1 ;i<50;i++){
+                NuovaFatturaDTO nuovaFatturaDTO = new NuovaFatturaDTO(i+""+i+""+i,10+i,i, StatoFattura.EMESSA);
+               fattureService.salva(nuovaFatturaDTO);
             }
         } catch (Exception ex) {
             ex.printStackTrace();
